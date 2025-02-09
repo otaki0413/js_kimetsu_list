53e9f9c03864b2b3cda8563441dec8cae78f580b
otaki0413 <takida0413@gmail.com>
2025-02-09 23:23:10 +0900

    🔧 定数の定義

---
+++ js/const.js
@@ -0,0 +1,6 @@
+// API関連
+export const BASE_URL = 'https://ihatov08.github.io';
+export const API_URL = `${BASE_URL}/kimetsu_api/api`;
+
+// 有効なカテゴリ
+export const VALID_CATEGORIES = ['all', 'kisatsutai', 'hashira', 'oni'];
